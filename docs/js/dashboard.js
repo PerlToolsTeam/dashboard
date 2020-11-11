be0287f74bc773e22d8b8dfc9453aaf5931988ef
Dave Cross <dave@dave.org.uk>
2020-11-11 16:10:34 +0000

    Add column sorting

--- docs/js/dashboard.js
+++ docs/js/dashboard.js
@@ -3,4 +3,12 @@ $(document).ready(function()
     $(".backup_picture").on("error", function(){
         $(this).attr('src', '/images/missing_image.png');
     });
+
+    $('#sort_table').DataTable({
+      "paging": false,
+      "columnDefs": [
+        { "targets": [0, 2], "orderable": true },
+        { "targets": "_all", "orderable": false },
+      ]
+    });
 });
